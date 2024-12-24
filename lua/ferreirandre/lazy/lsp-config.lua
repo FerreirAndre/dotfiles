ae5ab89d593dcc174d28f4c06d7be0f665338669
FerreirAndre <adrecruz15@gmail.com>
2024-12-24 11:56:30 -0300

    adding completions and connections to lsp

--- lua/ferreirandre/lazy/lsp-config.lua
+++ lua/ferreirandre/lazy/lsp-config.lua
@@ -3,26 +3,41 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { 'lua_ls', 'gopls', 'clangd' }
+                ensure_installed = { "lua_ls", "gopls", "clangd" },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.gopls.setup({})
-            vim.keymap.set('n','K',vim.lsp.buf.hover,{})
-            vim.keymap.set('n','gd', vim.lsp.buf.definition,{})
-            vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
-        end
-    }
+            --            local on_attach = require("plugins.configs.lspconfig").on_attach
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.gopls.setup({
+                --                on_attach = on_attach,
+                capabilities = capabilities,
+                cmd = {"gopls"},
+                filetypes = {"go","gomod","gowork","gotmpl"},
+                settings = {
+                    completeUnimported = true,
+                }
+            })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
